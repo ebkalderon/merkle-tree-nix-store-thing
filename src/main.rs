225f969123e00217cf2a011da90c5f7d41014fd2
Eyal Kalderon <ebkalderon@gmail.com>
2020-10-29 00:50:13 +0800

    Move PagedBuffer to src/util.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 use std::collections::{BTreeMap, BTreeSet, HashSet};
 use std::fmt::{self, Debug, Display, Formatter};
 use std::hash::Hash;
-use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+use std::io::{Read, Write};
 use std::os::unix::fs::{MetadataExt, PermissionsExt};
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
@@ -11,79 +11,9 @@ use anyhow::{anyhow, Context};
 use filetime::FileTime;
 use serde::{de::Deserializer, ser::Serializer, Deserialize, Serialize};
 
-// Utility
+use self::util::PagedBuffer;
 
-#[derive(Debug)]
-enum Storage {
-    Inline(Cursor<Box<[u8]>>, usize),
-    File(tempfile::NamedTempFile),
-}
-
-#[derive(Debug)]
-struct PagedBuffer {
-    inner: Storage,
-    threshold: usize,
-}
-
-impl PagedBuffer {
-    pub fn with_threshold(t: usize) -> Self {
-        let fixed_buf = Cursor::new(vec![0; t].into_boxed_slice());
-        PagedBuffer {
-            inner: Storage::Inline(fixed_buf, 0),
-            threshold: t,
-        }
-    }
-}
-
-impl Read for PagedBuffer {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        match self.inner {
-            Storage::Inline(ref mut b, _) => b.read(buf),
-            Storage::File(ref mut b) => b.read(buf),
-        }
-    }
-}
-
-impl Seek for PagedBuffer {
-    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
-        match self.inner {
-            Storage::Inline(ref mut b, _) => b.seek(pos),
-            Storage::File(ref mut b) => b.seek(pos),
-        }
-    }
-}
-
-impl Write for PagedBuffer {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        match self.inner {
-            Storage::Inline(ref mut b, ref mut bytes_written) => {
-                if *bytes_written + buf.len() > self.threshold {
-                    // TODO: Should we create this in a directory like `<store>/tmp` for security?
-                    let mut file = tempfile::NamedTempFile::new()?;
-                    std::io::copy(b, &mut file)?;
-                    file.as_file_mut().sync_all()?;
-
-                    let len = file.write(buf)?;
-                    self.inner = Storage::File(file);
-
-                    Ok(len)
-                } else {
-                    let len = b.write(buf)?;
-                    *bytes_written += len;
-                    Ok(len)
-                }
-            }
-            Storage::File(ref mut b) => b.write(buf),
-        }
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        match self.inner {
-            Storage::Inline(ref mut b, _) => b.flush(),
-            Storage::File(ref mut b) => b.flush(),
-        }
-    }
-}
+mod util;
 
 // Filesystem objects
 
---
+++ src/util.rs
@@ -0,0 +1,73 @@
+use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+
+#[derive(Debug)]
+enum Storage {
+    Inline(Cursor<Box<[u8]>>, usize),
+    File(tempfile::NamedTempFile),
+}
+
+#[derive(Debug)]
+pub struct PagedBuffer {
+    inner: Storage,
+    threshold: usize,
+}
+
+impl PagedBuffer {
+    pub fn with_threshold(t: usize) -> Self {
+        let fixed_buf = Cursor::new(vec![0; t].into_boxed_slice());
+        PagedBuffer {
+            inner: Storage::Inline(fixed_buf, 0),
+            threshold: t,
+        }
+    }
+}
+
+impl Read for PagedBuffer {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        match self.inner {
+            Storage::Inline(ref mut b, _) => b.read(buf),
+            Storage::File(ref mut b) => b.read(buf),
+        }
+    }
+}
+
+impl Seek for PagedBuffer {
+    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
+        match self.inner {
+            Storage::Inline(ref mut b, _) => b.seek(pos),
+            Storage::File(ref mut b) => b.seek(pos),
+        }
+    }
+}
+
+impl Write for PagedBuffer {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self.inner {
+            Storage::Inline(ref mut b, ref mut bytes_written) => {
+                if *bytes_written + buf.len() > self.threshold {
+                    // TODO: Should we create this in a directory like `<store>/tmp` for security?
+                    let mut file = tempfile::NamedTempFile::new()?;
+                    std::io::copy(b, &mut file)?;
+                    file.as_file_mut().sync_all()?;
+
+                    let len = file.write(buf)?;
+                    self.inner = Storage::File(file);
+
+                    Ok(len)
+                } else {
+                    let len = b.write(buf)?;
+                    *bytes_written += len;
+                    Ok(len)
+                }
+            }
+            Storage::File(ref mut b) => b.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        match self.inner {
+            Storage::Inline(ref mut b, _) => b.flush(),
+            Storage::File(ref mut b) => b.flush(),
+        }
+    }
+}
