6e1461d99577c73b3538a5accd250885216224ef
Eyal Kalderon <ebkalderon@gmail.com>
2020-10-29 15:27:16 +0800

    Use dynamically sized Vec<u8> for Storage::Inline
    It is actually a bug to use a zeroed `Box<[u8]>` initialized with a
    constant length in `PagedBuffer`, and we shouldn't do it. This is for
    two reasons:
    
    1. It's very inefficient to allocate the full size in-memory buffer up
       front like that, considering that some files are relatively small and
       may not take up much space. We shouldn't needlessly waste space.
    2. It's semantically wrong. While the `std::io::Write` implementation is
       able to correctly detect and handle the spillover from `Inline` to
       `File`, the `std::io::Read` implementation cannot. Because the length
       of the inline buffer is always == `threshold`, readers could
       potentially read past the end of the data and find a stream of `0u8`
       until EOF. Whoops!
    
    This commit fixes these issues by replacing `Box<[u8]>` with `Vec<u8>`
    as the in-memory buffer of choice, which we probably should've gone with
    in the first place. Although we don't currently know what a good
    up-front capacity for this `Vec` should be, it should at least fix any
    potential bugs with the `std::io::Read` implementation.

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ use std::io::{Cursor, Read, Seek, SeekFrom, Write};
 
 #[derive(Debug)]
 enum Storage {
-    Inline(Cursor<Box<[u8]>>, usize),
+    Inline(Cursor<Vec<u8>>),
     File(tempfile::NamedTempFile),
 }
 
@@ -14,9 +14,8 @@ pub struct PagedBuffer {
 
 impl PagedBuffer {
     pub fn with_threshold(t: usize) -> Self {
-        let fixed_buf = Cursor::new(vec![0; t].into_boxed_slice());
         PagedBuffer {
-            inner: Storage::Inline(fixed_buf, 0),
+            inner: Storage::Inline(Cursor::new(Vec::new())),
             threshold: t,
         }
     }
@@ -25,8 +24,8 @@ impl PagedBuffer {
 impl Read for PagedBuffer {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         match self.inner {
-            Storage::Inline(ref mut b, _) => b.read(buf),
-            Storage::File(ref mut b) => b.read(buf),
+            Storage::Inline(ref mut inner) => inner.read(buf),
+            Storage::File(ref mut inner) => inner.read(buf),
         }
     }
 }
@@ -34,8 +33,8 @@ impl Read for PagedBuffer {
 impl Seek for PagedBuffer {
     fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
         match self.inner {
-            Storage::Inline(ref mut b, _) => b.seek(pos),
-            Storage::File(ref mut b) => b.seek(pos),
+            Storage::Inline(ref mut inner) => inner.seek(pos),
+            Storage::File(ref mut inner) => inner.seek(pos),
         }
     }
 }
@@ -43,11 +42,11 @@ impl Seek for PagedBuffer {
 impl Write for PagedBuffer {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         match self.inner {
-            Storage::Inline(ref mut b, ref mut bytes_written) => {
-                if *bytes_written + buf.len() > self.threshold {
+            Storage::Inline(ref mut inner) => {
+                if inner.get_ref().len() + buf.len() > self.threshold {
                     // TODO: Should we create this in a directory like `<store>/tmp` for security?
                     let mut file = tempfile::NamedTempFile::new()?;
-                    std::io::copy(b, &mut file)?;
+                    std::io::copy(inner, &mut file)?;
                     file.as_file_mut().sync_data()?;
 
                     let len = file.write(buf)?;
@@ -55,19 +54,17 @@ impl Write for PagedBuffer {
 
                     Ok(len)
                 } else {
-                    let len = b.write(buf)?;
-                    *bytes_written += len;
-                    Ok(len)
+                    inner.write(buf)
                 }
             }
-            Storage::File(ref mut b) => b.write(buf),
+            Storage::File(ref mut inner) => inner.write(buf),
         }
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
         match self.inner {
-            Storage::Inline(ref mut b, _) => b.flush(),
-            Storage::File(ref mut b) => b.flush(),
+            Storage::Inline(ref mut inner) => inner.flush(),
+            Storage::File(ref mut inner) => inner.flush(),
         }
     }
 }
