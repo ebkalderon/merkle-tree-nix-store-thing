7a1a9679c19d097f69e20beacdf800cd427a4415
Eyal Kalderon <ebkalderon@gmail.com>
2020-11-04 02:49:27 +0800

    Implement Store::compute_delta() for copying closures
    In the process, we redefine the private `compute_closure()` function as
    a special-case of the more general `compute_delta_closure()`.

--- src/store.rs
+++ src/store.rs
@@ -141,6 +141,72 @@ pub trait Store {
             .map(|Ref(id, kind)| (id, kind))
             .collect())
     }
+
+    /// Computes a delta closure which only contains objects that are missing on the remote store.
+    ///
+    /// Returns `Err` if any of the given object IDs do not exist in this store, any of the object
+    /// IDs do not refer to a `Package` object, a cycle or structural inconsistency is detected in
+    /// the reference graph, or an I/O error occurred.
+    fn compute_delta(&self, pkgs: BTreeSet<ObjectId>, dest: &dyn Store) -> anyhow::Result<Closure> {
+        // This delta computation technique is shamelessly stolen from Git, as documented
+        // meticulously in these two pages:
+        //
+        // https://matthew-brett.github.io/curious-git/git_push_algorithm.html
+        // https://github.com/git/git/blob/master/Documentation/technical/pack-protocol.txt
+
+        // Use newtype because Rust disallows deriving/implementing these traits for tuples.
+        #[derive(Clone, Copy, Eq, Hash, PartialEq, PartialOrd, Ord)]
+        struct Ref(ObjectId, ObjectKind);
+
+        impl Display for Ref {
+            fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+                Display::fmt(&self.0, f)
+            }
+        }
+
+        let missing_pkgs = compute_delta_closure(pkgs, |id| {
+            let p = self.get_package(id)?;
+            if dest.contains_object(&id, Some(ObjectKind::Package))? {
+                Ok(Path::Abandon)
+            } else {
+                Ok(Path::Descend(p.references))
+            }
+        })?;
+
+        let mut trees = BTreeSet::new();
+        for id in &missing_pkgs {
+            let p = self.get_package(*id)?;
+            trees.insert(Ref(p.tree, ObjectKind::Tree));
+        }
+
+        let missing_content = compute_delta_closure(trees, |Ref(id, kind)| match kind {
+            ObjectKind::Blob | ObjectKind::Tree if dest.contains_object(&id, Some(kind))? => {
+                Ok(Path::Abandon)
+            }
+            ObjectKind::Blob => Ok(Path::Descend(BTreeSet::new())),
+            ObjectKind::Tree => {
+                let tree = self.get_tree(id)?;
+                let refs = tree.references();
+                Ok(Path::Descend(refs.map(|(id, k)| Ref(id, k)).collect()))
+            }
+            ObjectKind::Package => Err(anyhow!("tree object cannot reference package object")),
+        })?;
+
+        Ok(missing_pkgs
+            .into_iter()
+            .map(|id| Ref(id, ObjectKind::Package))
+            .chain(missing_content)
+            .map(|Ref(id, kind)| (id, kind))
+            .collect())
+    }
+}
+
+/// Control flow for the `get_children` closure.
+enum Path<T> {
+    /// Indicates that traversal should continue with the given child nodes.
+    Descend(BTreeSet<T>),
+    /// Indicates that traversal should halt and return a partial result.
+    Abandon,
 }
 
 /// Performs a depth-first search of a directed acyclic graph (DAG), descending from the given root
@@ -148,17 +214,33 @@ pub trait Store {
 /// a topologically sorted list of references, where if tree object `P` depends on `Q`, then `P` is
 /// ordered before `Q` in the list.
 ///
-/// This sorting is critical for quick and efficient delta computation between two different
-/// stores, like when copying a closure over the network to a store on a remote machine, or when
-/// querying remote machines for tree objects to download. This is shamelessly stolen from how Git
-/// does delta computation, as documented meticulously in this file:
-///
-/// https://github.com/git/git/blob/master/Documentation/technical/pack-protocol.txt
-fn compute_closure<T, F>(items: BTreeSet<T>, get_children: F) -> anyhow::Result<Vec<T>>
+/// This sorting is important because it ensures objects and packages get inserted into the store
+/// in a consistent order, where all references are inserted into the store before their referrers.
+fn compute_closure<T, F>(items: BTreeSet<T>, mut get_children: F) -> anyhow::Result<Vec<T>>
 where
     T: Copy + Display + Eq + Hash + Ord,
     F: FnMut(T) -> anyhow::Result<BTreeSet<T>>,
 {
+    compute_delta_closure(items, |item| get_children(item).map(Path::Descend))
+}
+
+/// Similar to `compute_closure()`, but with the option to halt early and return a partial result.
+///
+/// If `get_children` returns `Ok(Path::Descend(children))`, it indicates that we should descend
+/// further in the child nodes in a regular depth-first search. However, if `get_children`  returns
+/// `Ok(Path::Abandon)`, it means that we should stop descending any further and try again from a
+/// different root node, if any, returning only a partial result.
+fn compute_delta_closure<T, F>(items: BTreeSet<T>, get_children: F) -> anyhow::Result<Vec<T>>
+where
+    T: Copy + Display + Eq + Hash + Ord,
+    F: FnMut(T) -> anyhow::Result<Path<T>>,
+{
+    #[derive(PartialEq)]
+    enum Traversal {
+        Continue,
+        Halt,
+    }
+
     // Use a struct with fields and methods because recursive closures are impossible in Rust.
     struct ClosureBuilder<'a, T: Eq + Hash + Ord, F> {
         initial_items: &'a BTreeSet<T>,
@@ -171,7 +253,7 @@ where
     impl<'a, T, F> ClosureBuilder<'a, T, F>
     where
         T: Copy + Display + Eq + Hash + Ord,
-        F: FnMut(T) -> anyhow::Result<BTreeSet<T>>,
+        F: FnMut(T) -> anyhow::Result<Path<T>>,
     {
         pub fn new(initial_items: &'a BTreeSet<T>, get_children: F) -> Self {
             ClosureBuilder {
@@ -193,7 +275,7 @@ where
             Ok(self.topo_sorted_items)
         }
 
-        fn visit_dfs(&mut self, item: T, parent_item: Option<T>) -> anyhow::Result<()> {
+        fn visit_dfs(&mut self, item: T, parent_item: Option<T>) -> anyhow::Result<Traversal> {
             // Reference cycles are forbidden, so exit early if one is found.
             if self.parents.contains(&item) {
                 return Err(anyhow!(
@@ -205,16 +287,23 @@ where
 
             // Return early if we have already visited this node before.
             if !self.visited.insert(item) {
-                return Ok(());
+                return Ok(Traversal::Continue);
             }
+
+            // Decide whether to continue the DFS or abandon it in favor of the next initial item.
+            let children = match (self.get_children)(item)? {
+                Path::Descend(children) => children,
+                Path::Abandon => return Ok(Traversal::Halt),
+            };
 
             // Mark this node as a parent, to detect cycles.
             self.parents.insert(item);
 
             // Continue descending into the child nodes in a DFS, if any exist.
-            for child in (self.get_children)(item)? {
-                if child != item {
-                    self.visit_dfs(child, Some(item))?;
+            for child in children {
+                if self.visit_dfs(child, Some(item))? == Traversal::Halt {
+                    self.parents.remove(&item);
+                    return Ok(Traversal::Halt);
                 }
             }
 
@@ -222,7 +311,7 @@ where
             self.topo_sorted_items.push(item);
             self.parents.remove(&item);
 
-            Ok(())
+            Ok(Traversal::Continue)
         }
     }
 
