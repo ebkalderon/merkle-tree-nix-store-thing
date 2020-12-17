8864dfc909f3b45b90a646299d3cafb190a3a8e5
Eyal Kalderon <ebkalderon@gmail.com>
2020-12-17 16:37:48 +0800

    Move Store into local.rs, move Objects into root
    This is the beginning of an effort to transition `Store` functionality
    into a trait, rather than a struct, and also expose `Objects` as a
    top-level trait for use with store-like things that can't build packages
    from source and only contain objects, e.g. binary caches, to be added in
    the future.

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,10 @@
 
 pub use self::closure::Closure;
 pub use self::copy::copy_closure;
+pub use self::local::{Backend, Store};
 pub use self::object::*;
 
-use std::collections::BTreeSet;
-use std::path::PathBuf;
-
-use crate::copy::{Delta, Destination, Progress as CopyProgress, Source};
-use crate::local::{Backend, Filesystem, Objects, Packages};
+use anyhow::anyhow;
 
 pub mod copy;
 
@@ -18,122 +15,85 @@ mod local;
 mod object;
 mod util;
 
-/// A content-addressable store of installed software packages.
-#[derive(Debug)]
-pub struct Store<B: Backend = Filesystem> {
-    objects: B::Objects,
-    packages: B::Packages,
-}
-
-impl<B: Backend> Store<B> {
-    /// Opens the store on the directory located in `path`.
-    ///
-    /// Returns `Err` if the path does not exist or is not a valid store directory.
-    pub fn open<P: Into<PathBuf>>(path: P) -> anyhow::Result<Self> {
-        let (objects, packages) = B::open(path.into())?;
-        Ok(Store { objects, packages })
-    }
-
-    /// Initializes a new store directory at `path` and opens it.
-    ///
-    /// If an empty target directory does not already exist at that location, it will be
-    /// automatically created. If a store directory already exists at that location, it will be
-    /// opened.
-    ///
-    /// Returns `Err` if `path` exists and does not point to a valid store directory, or if a new
-    /// store directory could not be created at `path` due to permissions or other I/O errors.
-    pub fn init<P: Into<PathBuf>>(path: P) -> anyhow::Result<Self> {
-        let (objects, packages) = B::init(path.into())?;
-        Ok(Store { objects, packages })
-    }
-
-    /// Initializes a store inside the empty directory referred to by `path` and opens it.
-    ///
-    /// If a store directory already exists at that location, it will be opened.
-    ///
-    /// Returns `Err` if `path` exists and does not point to a valid store directory or an empty
-    /// directory, or the new store directory could not be initialized at `path` due to permissions
-    /// or I/O errors.
-    pub fn init_bare<P: Into<PathBuf>>(path: P) -> anyhow::Result<Self> {
-        let (objects, packages) = B::init_bare(path.into())?;
-        Ok(Store { objects, packages })
-    }
-
+/// A content-addressable repository of Merkle tree objects.
+pub trait Objects {
     /// Inserts a tree object into the store, returning its unique ID.
     ///
-    /// Returns `Err` if the object could not be inserted into the store or an I/O error occurred.
-    pub fn insert_object(&mut self, o: Object) -> anyhow::Result<ObjectId> {
-        if let Object::Package(ref pkg) = &o {
-            self.packages.install(pkg, &self.objects)?;
-        }
-
-        self.objects.insert_object(o)
-    }
-}
-
-impl<B: Backend> Store<B> {
-    /// Computes the filesystem closure for the given packages.
+    /// Implementers _must_ ensure that this method behaves as a completely atomic transaction.
+    /// Implementers _should_ take care to memoize this method such that if the object already
+    /// exists in the store, this method does nothing.
     ///
-    /// Returns `Err` if any of the given object IDs do not exist, any of the object IDs do not
-    /// refer to a `Package` object, a cycle or structural inconsistency is detected in the
-    /// reference graph, or an I/O error occurred.
-    #[inline]
-    pub fn compute_closure(&self, pkgs: BTreeSet<ObjectId>) -> anyhow::Result<Closure> {
-        closure::compute(&self.objects, pkgs, |_id, _kind| Ok(true))
-    }
-}
+    /// Returns `Err` if the object could not be inserted into the store or an I/O error occurred.
+    fn insert_object(&mut self, o: Object) -> anyhow::Result<ObjectId>;
 
-impl<'s, B: Backend> Source<'s> for Store<B> {
-    type Objects = Box<dyn Iterator<Item = anyhow::Result<Object>> + 's>;
+    /// Looks up a specific tree object in the store and retrieves it, if it exists.
+    ///
+    /// If the type of the requested object is known up-front, implementers _can_ use this detail
+    /// to locate and retrieve the object faster. Otherwise, callers can specify `None` and the
+    /// store will attempt to guess the desired object type, if it is not immediately known.
+    ///
+    /// Returns `Err` if the object does not exist or an I/O error occurred.
+    fn get_object(&self, id: ObjectId, kind: Option<ObjectKind>) -> anyhow::Result<Object>;
 
-    fn find_missing<D>(&self, dst: &D, pkgs: BTreeSet<ObjectId>) -> anyhow::Result<Delta>
-    where
-        D: Destination + ?Sized,
-    {
-        // This delta computation technique was shamelessly stolen from Git, as documented
-        // meticulously in these two pages:
-        //
-        // https://matthew-brett.github.io/curious-git/git_push_algorithm.html
-        // https://github.com/git/git/blob/master/Documentation/technical/pack-protocol.txt
+    /// Returns `true` if the store contains a tree object with the given unique ID, or `false`
+    /// otherwise.
+    ///
+    /// If the type of the requested object is known up-front, implementers _can_ use this detail
+    /// to locate and retrieve the object faster. Otherwise, callers can specify `None` and the
+    /// store will attempt to guess the desired object type, if it is not immediately known.
+    fn contains_object(&self, id: &ObjectId, kind: Option<ObjectKind>) -> bool;
 
-        let mut num_present = 0;
-        let missing = closure::compute(&self.objects, pkgs, |id, kind| {
-            let exists = dst.contains_object(&id, Some(kind))?;
-            if exists {
-                num_present += 1;
-            }
-            Ok(!exists)
-        })?;
+    /// Returns the on-disk size of the object in bytes, if it exists in the store.
+    ///
+    /// If the type of the requested object is known up-front, implementers _can_ use this detail
+    /// to locate and retrieve the object faster. Otherwise, callers can specify `None` and the
+    /// store will attempt to guess the desired object type, if it is not immediately known.
+    ///
+    /// Returns `Err` if the object does not exist or an I/O error occurred.
+    fn object_size(&self, id: &ObjectId, kind: Option<ObjectKind>) -> anyhow::Result<u64>;
 
-        Ok(Delta {
-            num_present,
-            missing,
+    /// Looks up a `Blob` object with the given ID and retrieves it, if it exists.
+    ///
+    /// Returns `Err` if the object does not exist, the given ID does not refer to a `Blob` object,
+    /// or an I/O error occurred.
+    fn get_blob(&self, id: ObjectId) -> anyhow::Result<Blob> {
+        self.get_object(id, Some(ObjectKind::Blob)).and_then(|o| {
+            o.into_blob()
+                .map_err(|_| anyhow!("{} is not a blob object", id))
         })
     }
 
-    fn yield_objects(&'s self, closure: Closure) -> anyhow::Result<Self::Objects> {
-        Ok(Box::new(closure.sort_yield().into_iter().map(
-            move |(id, kind, _)| self.objects.get_object(id, Some(kind)),
-        )))
-    }
-}
-
-impl<B: Backend> Destination for Store<B> {
-    type Progress = std::iter::Empty<anyhow::Result<CopyProgress>>;
-
-    fn contains_object(&self, id: &ObjectId, kind: Option<ObjectKind>) -> anyhow::Result<bool> {
-        Ok(self.objects.contains_object(id, kind))
+    /// Looks up a `Tree` object with the given ID and retrieves it, if it exists.
+    ///
+    /// Returns `Err` if the object does not exist, the given ID does not refer to a `Tree` object,
+    /// or an I/O error occurred.
+    fn get_tree(&self, id: ObjectId) -> anyhow::Result<Tree> {
+        self.get_object(id, Some(ObjectKind::Tree)).and_then(|o| {
+            o.into_tree()
+                .map_err(|_| anyhow!("{} is not a tree object", id))
+        })
     }
 
-    fn insert_objects<I>(&mut self, stream: I) -> anyhow::Result<Self::Progress>
-    where
-        I: Iterator<Item = anyhow::Result<Object>>,
-    {
-        for result in stream {
-            let obj = result?;
-            self.insert_object(obj)?;
-        }
+    /// Looks up a `Package` object with the given ID and retrieves it, if it exists.
+    ///
+    /// Returns `Err` if the object does not exist, the given ID does not refer to a `Package`
+    /// object, or an I/O error occurred.
+    fn get_package(&self, id: ObjectId) -> anyhow::Result<Package> {
+        self.get_object(id, Some(ObjectKind::Package))
+            .and_then(|o| {
+                o.into_package()
+                    .map_err(|_| anyhow!("{} is not a package object", id))
+            })
+    }
 
-        Ok(std::iter::empty())
+    /// Looks up a `Spec` object with the given ID and retrieves it, if it exists.
+    ///
+    /// Returns `Err` if the object does not exist, the given ID does not refer to a `Spec` object,
+    /// or an I/O error occurred.
+    fn get_spec(&self, id: ObjectId) -> anyhow::Result<Spec> {
+        self.get_object(id, Some(ObjectKind::Spec)).and_then(|o| {
+            o.into_spec()
+                .map_err(|_| anyhow!("{} is not a spec object", id))
+        })
     }
 }
